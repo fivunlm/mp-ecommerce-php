e3830d5fc8172fb6aeb6674285119d6eeb237a7b
Fernando <lopez.fernando.damian@gmail.com>
2020-08-21 15:36:04 -0300

    Uso error log

--- notifications.php
+++ notifications.php
@@ -1,5 +1,3 @@
 <?php
 $jsonString = file_get_contents("php://input");
-$out = fopen('php://output', 'w');
-fputs($out, $jsonString); //writing output operation
-fclose($out);
+error_log($jsonString);
